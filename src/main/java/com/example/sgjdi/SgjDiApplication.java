030e0e7d82e74c237b6df677eb08bfac35e199e8
Ronnen Nagal <nagalr@gmail.com>
2020-12-07 12:01:09 +0200

    Initial commit

---
+++ src/main/java/com/example/sgjdi/SgjDiApplication.java
@@ -0,0 +1,15 @@
+package com.example.sgjdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgjDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgjDiApplication.class, args);
+	}
+
+}
+
+
---
+++ src/test/java/com/example/sgjdi/SgjDiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sgjdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgjDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
